d6f879e0a02024a4a32819885c33d0a5fa7b2d51
Matt Zhang <mcmattzhang@gmail.com>
2020-12-20 00:34:36 -0500

    random controls added

--- snake.js
+++ snake.js
@@ -7,7 +7,24 @@ document.addEventListener("keydown", changeDir)
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d");
 
+let upDisplay = document.getElementById("up");
+let leftDisplay = document.getElementById("left");
+let downDisplay = document.getElementById("down");
+let rightDisplay = document.getElementById("right");
+
 let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
+let foodX = randomCoord(0,gameCanvas.width-10);
+let foodY = randomCoord(0,gameCanvas.height-10);
+let xSpeed = 0;
+let ySpeed = 0;
+let gameSpeed = 100;
+let xDir = 1;
+let yDir = 0;
+
+let LEFT_KEY;
+let RIGHT_KEY;
+let UP_KEY;
+let DOWN_KEY;
 
 function clearCanvas() {
     ctx.fillStyle = boardBackground;
@@ -26,13 +43,6 @@ function drawSnakePart(snakePart) {
 function drawSnake() {
     snake.forEach(drawSnakePart);
 }
-let foodX = randomCoord(0,gameCanvas.width-10);
-let foodY = randomCoord(0,gameCanvas.height-10);
-let xSpeed = 10;
-let ySpeed = 10;
-let gameSpeed = 100;
-let xDir = 1;
-let yDir = 0;
 
 function main() {
     setTimeout(() => {
@@ -69,22 +79,33 @@ function isGameOver() {
 function update() {
     const head = {x: snake[0].x+xSpeed*xDir, y:snake[0].y+ySpeed*yDir};
     snake.unshift(head);
-    console.log(foodX,foodY,head.x,head.y) 
+    // console.log(foodX,foodY,head.x,head.y) 
     if (onXY(foodX,foodY,snake[0])) newFood();
     else snake.pop();
 }   
 
 function changeDir() {
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39;
-    const UP_KEY = 38;
-    const DOWN_KEY = 40;
-
     const keyPressed = event.keyCode;
     if (keyPressed === LEFT_KEY && xDir != 1) {xDir = -1; yDir = 0;}
     if (keyPressed === RIGHT_KEY && xDir != -1) {xDir = 1; yDir = 0;}
     if (keyPressed === UP_KEY && yDir != 1) {xDir = 0; yDir = -1;}
     if (keyPressed === DOWN_KEY && yDir != -1) {xDir = 0; yDir = 1;}
+
+    var arr = [];
+    while(arr.length < 4){
+        var r = Math.floor(Math.random() * 26) + 65;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+
+    LEFT_KEY = arr[0];
+    RIGHT_KEY = arr[1];
+    UP_KEY = arr[2];
+    DOWN_KEY = arr[3];
+
+    upDisplay.innerHTML=String.fromCharCode(UP_KEY);
+    leftDisplay.innerHTML=String.fromCharCode(LEFT_KEY);
+    downDisplay.innerHTML=String.fromCharCode(DOWN_KEY);
+    rightDisplay.innerHTML=String.fromCharCode(RIGHT_KEY);
 }
 
 function randomCoord(min, max) {
@@ -110,4 +131,31 @@ function newFood() {
     })
 }
 
-main();
+function startGame() {
+    snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
+    foodX = randomCoord(0,gameCanvas.width-10);
+    foodY = randomCoord(0,gameCanvas.height-10);
+    xSpeed = 10;
+    ySpeed = 10;
+    gameSpeed = 100;
+    xDir = 1;
+    yDir = 0;
+    var arr = [];
+    while(arr.length < 4){
+        var r = Math.floor(Math.random() * 26) + 65;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+
+    LEFT_KEY = arr[0];
+    RIGHT_KEY = arr[1];
+    UP_KEY = arr[2];
+    DOWN_KEY = arr[3];
+
+    upDisplay.innerHTML=String.fromCharCode(UP_KEY);
+    leftDisplay.innerHTML=String.fromCharCode(LEFT_KEY);
+    downDisplay.innerHTML=String.fromCharCode(DOWN_KEY);
+    rightDisplay.innerHTML=String.fromCharCode(RIGHT_KEY);
+    main();
+}
+
+startGame();
